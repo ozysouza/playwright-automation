69a6bdcc5a98998275042110a3e7aca97418c48a
ozysouza <ozysouzaa@gmail.com>
2026-01-18 18:41:50 -0500

    Refact: Inject customExpect on fixture

--- pw-conduit-api/fixtures/requestHandlerTest.ts
+++ pw-conduit-api/fixtures/requestHandlerTest.ts
@@ -2,6 +2,7 @@ import { test as base } from "@playwright/test";
 import { RequestHandler } from "../helpers/api/requestHandler";
 import { config } from "../config/api-test.config";
 import { APILogger } from '../utils/logger'
+import { setCustomExpectLogger } from '../utils/customExpect'
 
 export type TestFixtures = {
     requestHandler: RequestHandler
@@ -10,6 +11,7 @@ export type TestFixtures = {
 export const test = base.extend<TestFixtures>({
     requestHandler: async ({ request }, use) => {
         const logger = new APILogger()
+        setCustomExpectLogger(logger)
         await use(new RequestHandler(request, config.apiUrl, logger))
     }
 })
