08ced819e2d7fe70fc12fff1efea4ca398617eae
ozysouza <ozysouzaa@gmail.com>
2026-01-20 21:09:08 -0500

    test: Create test to get global articles list with pagination

--- pw-conduit-api/tests/api/articles/articles-read.spec.ts
+++ pw-conduit-api/tests/api/articles/articles-read.spec.ts
@@ -3,6 +3,7 @@ import { requestHandlerTest } from '../../../fixtures/requestHandlerTest'
 import { assertApiTest } from '../../../fixtures/assertApiTest';
 import { apiExpect } from '../../../utils/customExpect';
 import globalPayload from '../../../request-objects/articles/GLOBAL_GET_articles.json'
+import pagGlobalPayload from '../../../request-objects/articles/PAG_GLOBAL_GET_articles.json'
 
 export const test = mergeTests(
     requestHandlerTest,
@@ -46,3 +47,33 @@ test('Get global articles list', async ({ requestHandler, assertApi }) => {
     })
 })
 
+test('Get global articles list with pagination', async ({ requestHandler, assertApi }) => {
+    let response: any
+
+    await test.step('Given the user is unauthenticated', async () => {
+        requestHandler.clearAuth()
+    })
+
+    await test.step('When the user requests paginated global articles', async () => {
+        response = await requestHandler
+            .path('/articles')
+            .params({ limit: 3, offset: 5 })
+            .getRequest(200)
+    })
+
+    await test.step('Then the response should match the articles schema', async () => {
+        await apiExpect(response).toMatchSchema('articles', 'GET_articles')
+    })
+
+    await test.step('And pagination should return three articles', async () => {
+        assertApi.articlesPagination(response.articles, 3)
+    })
+
+    await test.step('And each article should have the required fields', async () => {
+        assertApi.articlesProperties(response.articles)
+    })
+
+    await test.step('And the response should match the expected paginated payload', async () => {
+        assertApi.articlesMatch(response.articles, pagGlobalPayload.articles)
+    })
+})
