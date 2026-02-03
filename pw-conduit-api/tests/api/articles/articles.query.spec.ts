10e71c2f6f0e3e88704b95baee5260621cf3b26e
ozysouza <ozysouzaa@gmail.com>
2026-02-02 21:39:09 -0500

    test: Should return 401 when updating an article without authentication

--- pw-conduit-api/tests/api/articles/articles.query.spec.ts
+++ pw-conduit-api/tests/api/articles/articles.query.spec.ts
@@ -202,6 +202,35 @@ test.describe('Articles - Update (PUT) Operations', {
         })
     })
 
+    test('Should return 401 when updating an article without authentication', async ({ requestHandler, assertApi }) => {
+        let requestResponse: any
+
+        await test.step('Given the user is not authenticated', async () => {
+            requestResponse = await requestHandler
+                .clearAuth()
+        })
+
+        await test.step('When the user attempts to update a new article', async () => {
+            const payload = buildArticlePayload()
+
+            requestResponse = await requestHandler
+                .path('/articles/invalid-slug')
+                .body(payload)
+                .putRequest(401)
+        })
+
+        await test.step('Then the API should respond with 401 and an authorization error message', async () => {
+            expect(requestResponse).toMatchObject({
+                status: 'error',
+                message: 'missing authorization credentials'
+            })
+        })
+
+        await test.step('And the response should match the 401 error schema', async () => {
+            await apiExpect(requestResponse).toMatchSchema('errors', '401_auth_article')
+        })
+    })
+
     test('Should return 404 when updating an article with an invalid slug', async ({ requestHandler, assertApi }) => {
         let requestResponse: any
         const invalidSlug = 'this-is-invalid-123'
@@ -299,7 +328,7 @@ test.describe('Articles - Create (POST) Operations', {
         })
 
         await test.step('And the response should match the 401 error schema', async () => {
-            await apiExpect(requestResponse).toMatchSchema('errors', 'POST_401_auth_article')
+            await apiExpect(requestResponse).toMatchSchema('errors', '401_auth_article')
         })
     })
 
